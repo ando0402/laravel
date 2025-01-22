8d78d289b1a66a85ab580e89f0babe970ce1329c
kazuto ando <kazuto.ando@gmail.com>
2025-01-22 12:43:00 +0900

    add messages show

---
+++ app/app/Http/Controllers/MessageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Message;
+
+class MessageController extends Controller
+{
+    //
+    public function index(): view
+    {
+        // メッセージテーブルのレコードを全件取得
+        $messages = Message::all();
+        // messagesというキーで、ビューへ渡す
+        return view('message/index', ['messages' => $messages]);
+    }
+}

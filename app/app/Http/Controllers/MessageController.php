97f5e9670199138ffe5ae632587cc222d6f6ad85
kazuto ando <kazuto.ando@gmail.com>
2025-01-22 14:36:24 +0900

    add message registration

--- app/app/Http/Controllers/MessageController.php
+++ app/app/Http/Controllers/MessageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use App\Models\Message;
 
+
+
 class MessageController extends Controller
 {
-    //
+
     public function index(): view
     {
         // メッセージテーブルのレコードを全件取得
@@ -16,4 +19,19 @@ class MessageController extends Controller
         // messagesというキーで、ビューへ渡す
         return view('message/index', ['messages' => $messages]);
     }
+
+    /*
+     * 登録処理
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        // リクエストからボディを取得し、保存
+        $message = new Message();
+        $message->body = $request->input('body');;
+        $message->save();
+
+        // 処理後、リダイレクト
+        return redirect('/messages');
+    }
+
 }
--- app/app/Models/Message.php
+++ app/app/Models/Message.php
@@ -5,8 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $body
+ */
 class Message extends Model
 {
     //
     use HasFactory;
+
 }

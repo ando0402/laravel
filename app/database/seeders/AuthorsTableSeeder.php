b7e9e67cf714e28432ee11d6885d4afad7762f13
kazuto ando <kazuto.ando@gmail.com>
2025-01-23 09:10:06 +0900

    add AuthorsDetail Seeder

--- app/database/seeders/AuthorsTableSeeder.php
+++ app/database/seeders/AuthorsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Factories\AuthorDetailFactory;
 use Database\Factories\AuthorFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,6 +17,9 @@ class AuthorsTableSeeder extends Seeder
     {
         // ファクトリによって5件の著作情報を作成
         AuthorFactory::new()->count(5)->create();
+        // ファクトリによって5件の著作詳細情報を作成
+        // 合わせて著者情報も5件作成される
+        AuthordetailFactory::new()->count(5)->create();
 //        Author::factory(5)->create();
     }
 }

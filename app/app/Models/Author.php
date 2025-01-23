832a7ef9d304a5de545129b6adbaa17498822c27
kazuto ando <kazuto.ando@gmail.com>
2025-01-23 12:43:53 +0900

    relation one_to_one Author main hasone  AuthorDetail follow belongsTo

--- app/app/Models/Author.php
+++ app/app/Models/Author.php
@@ -5,11 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Author extends Model
 {
     //
     use HasFactory;
+
+
+    // 1対1
+    // 1対1のリレーション　主となるクラス hasOneメソッド
+    public function detail(): HasOne
+    {
+        return $this->hasOne(AuthorDetail::class);
+    }
 
     // 多対多
     // 多の方　belongsTo
--- app/app/Models/AuthorDetail.php
+++ app/app/Models/AuthorDetail.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AuthorDetail extends Model
 {
+    use HasFactory;
     //
+
+    // ID以外を主キーにしている場合は$primaryKeyを定義する
+    protected $primaryKey = 'author_id';
+
+    // 1対1 主Authorモデル
+    // 従となるモデルAuthorDetail
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
 }
